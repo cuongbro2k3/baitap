46d62cb6411da3b538a17049b9d250ab8976dae8
Trần Quang Cường <cuongid241@gmail.com>
2025-04-25 11:10:37 +0700

    lab2

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,11 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name='index' options={{ headerShown: false }} />
+      <Stack.Screen name='lab2' options={{ headerShown: false }} />
       <Stack.Screen name='bai1' options={{ headerShown: false }} />
       <Stack.Screen name='bai2' options={{ headerShown: false }} />
+      <Stack.Screen name='lab1' options={{ headerShown: false }} />
+      <Stack.Screen name='userLab2' options={{ headerShown: false }} />
     </Stack>
   )
 }
